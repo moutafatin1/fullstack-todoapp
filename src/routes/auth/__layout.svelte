004486040c081bed65336ebfc8298360ee4b9d28
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-25 10:26:21 +0100

    layouts

---
+++ src/routes/auth/__layout.svelte
@@ -0,0 +1,11 @@
+<div class="grid lg:grid-cols-2">
+	<div
+		class="hidden bg-primary text-center justify-center items-center space-y-7 flex-col pt-24 lg:flex"
+	>
+		<h1 class="text-gray-50 font-bold text-4xl ">
+			Welcome To My Awesome Todo Application
+		</h1>
+		<img src="/taskSvg.svg" alt="" class="w-1/2 drop-shadow-lg" />
+	</div>
+	<slot />
+</div>
