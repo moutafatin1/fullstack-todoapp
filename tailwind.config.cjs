7610e469dbcab5288be44ee5cd17b3a895bccbd6
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-25 10:20:55 +0100

    initialize new sveltekit project with tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#53C5B9',
+				darkGray: '#3B3E52'
+			}
+		}
+	},
+	plugins: []
+};
