b5f884ded2dc3a641a19b4a4b92f7b71b659ee82
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-25 10:26:58 +0100

    register page

---
+++ src/routes/auth/register/index.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import NavLink from '$root/components/auth/NavLink.svelte';
+	import Button from '$root/components/elements/button/Button.svelte';
+	import TextInput from '$root/components/elements/form/TextInput.svelte';
+	import { TextType } from '$root/components/elements/form/type';
+	import { User, Mail, Lock } from 'lucide-svelte';
+
+	let username = '';
+	let email = '';
+	let password = '';
+</script>
+
+<main class="flex flex-col items-center h-screen bg-darkGray ">
+	<NavLink />
+	<form
+		class="flex flex-col items-center
+	justify-center h-full space-y-7 w-full px-10"
+	>
+		<TextInput
+			name="username"
+			type={TextType.text}
+			bind:value={username}
+			icon={User}
+		/>
+		<TextInput
+			name="email"
+			type={TextType.email}
+			bind:value={email}
+			icon={Mail}
+		/>
+		<TextInput
+			name="password"
+			type={TextType.password}
+			bind:value={password}
+			icon={Lock}
+		/>
+		<div class="flex flex-col space-y-2 text-center">
+			<Button size={'md'} class="rounded-full font-bold">Sign Up</Button>
+			<a
+				href="/auth/login"
+				class="text-gray-400 border-b border-gray-400 hover:text-primary hover:border-primary transition-colors md:text-lg"
+				>I'm already member</a
+			>
+		</div>
+	</form>
+</main>
