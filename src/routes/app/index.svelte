1f5a941cbe8a25d02262352577d539edfd0162b3
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-29 10:47:43 +0100

    add different routes to  start working on the app

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -1,7 +1,29 @@
-<script>
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = ({ session, props }) => {
+		if (!session.user) {
+			return {
+				status: 302,
+				redirect: '/auth/login'
+			};
+		}
+
+		return {
+			status: 200,
+			props: {
+				user: session.user.username
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
 	import { MenuIcon } from '$root/components/icons';
 
 	import { handleSidebar } from '$root/lib/stores/sideBar';
+
+	export let user: string;
 </script>
 
 <main class="h-screen w-full">
@@ -15,5 +37,5 @@
 			/>
 		</button>
 	</div>
-	<h1>Hello to you dashboard</h1>
+	<h1>Hello to you dashboard {user}</h1>
 </main>
