a6e6fd71f5f89c4238fc637b619812bb1b32a9d6
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-29 10:44:42 +0100

    click logo redirect to home page

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,8 +1,13 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	import { CheckSquareIcon } from './icons';
 </script>
 
-<div class="flex items-center space-x-2 text-2xl text-primary ">
+<div
+	class="flex items-center space-x-2 text-2xl text-primary cursor-pointer"
+	on:click={() => goto('/')}
+>
 	<CheckSquareIcon class="h-10 w-10" />
 	<span class="text-gray-300 pointer-events-none">Epic Todo</span>
 </div>
