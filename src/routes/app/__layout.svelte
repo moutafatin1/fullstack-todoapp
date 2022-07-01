a033d33e0c249c2b0066da0e70b6fbf8ea8fd6b3
Oussama Moutafatin <moutafatin3@gmail.com>
2022-07-01 19:09:57 +0100

    add header and fix style

--- src/routes/app/__layout.svelte
+++ src/routes/app/__layout.svelte
@@ -12,17 +12,20 @@
 	<!-- Desktop Sidebar -->
 
 	<main class="h-screen w-full">
-		<div class="flex  p-2 md:hidden">
+		<div
+			class="flex flex-col justify-center fixed w-full p-2  bg-gray-500/50   md:h-16 "
+		>
 			<button
 				on:click={handleSidebar}
-				class="focus:ring-2 focus:ring-gray-500 focus:outline-none rounded-md"
+				class="focus:ring-2 focus:ring-gray-500 focus:outline-none rounded-md w-fit md:hidden"
 			>
 				<MenuIcon
 					class="h-8 w-8 cursor-pointer hover:text-gray-500 active:scale-95 "
 				/>
 			</button>
+			<h1 class="text-2xl text-gray-800 font-bold">All Tasks</h1>
 		</div>
-		<div>
+		<div class="mt-6 md:mt-0">
 			<slot />
 		</div>
 	</main>
